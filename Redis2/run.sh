b08b41aa56936beb6e7b3bc14a71d0fd79dc7c32
wjw465150 <wjw465150@gmail.com>
2015-09-10 11:58:43 +0800

    在Redis2的运行脚本里run.sh添加上注释: #echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf #echo "net.core.somaxconn = 1024" >> /etc/sysctl.conf #sysctl -p

--- Redis2/run.sh
+++ Redis2/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+#echo "net.core.somaxconn = 1024" >> /etc/sysctl.conf
+#sysctl -p
+
 /opt/redis/bin/redis-server /opt/redis/db/redis.conf
 
 # infinite loop
